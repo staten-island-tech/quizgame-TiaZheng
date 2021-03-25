2256e929fd425aebaaeebb0e174f0ac7f428416a
TiaZheng <tia.zheng22@sitechhs.com>
2021-03-24 22:06:57 -0400

    vscode being weird, need to redo!!!

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 console.log("Hello")
+
+const start = document.getElementById("start");
+
+const quiz = document.getElementById("question-container");
+
+const question = document.getElementById("question");
+
+const counter = document.getElementById("counter");
+
+const timeGauge = document.getElementById("timeGauge");
+
+const choice1 = document.getElementById("1");
+const choice2 = document.getElementById("2");
+const choice3 = document.getElementById("3");
+
+con
