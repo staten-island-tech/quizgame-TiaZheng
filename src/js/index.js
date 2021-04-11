9364c3aa683793046bf0047c6ca80f9c8bb0b3ac
TiaZheng <tia.zheng22@sitechhs.com>
2021-04-10 21:01:53 -0400

    x

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 console.log("Hello")
 
-const start = document.getElementById("start");
+/*const start = document.getElementById("start");
 
-const quiz = document.getElementById("question-container");
+/*const quiz = document.getElementById("question-container");
 
 const question = document.getElementById("question");
 
@@ -17,3 +17,13 @@ const choice3 = document.getElementById("3");
 const progress = document.getElementById("progress");
 
 const scoreContainer = document.getElementById("scoreContanier");
+
+
+const start = document.getElementById("start");
+*/
+
+(favNumber = function(num = 3) {
+    console.log("My favorite number is" + num);
+})();
+
+favNumber(5);
